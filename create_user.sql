a1c1c144b88e2fc94b6c2919fb97284910c87ffc
crck2rich <crck2rich@gmail.com>
2026-02-13 00:47:57 +0100

    Update create_user.sql

--- create_user.sql
+++ create_user.sql
@@ -1,4 +1,1 @@
-DROP USER IF EXISTS 'vaultuser'@'localhost';
-CREATE USER 'vaultuser'@'localhost' IDENTIFIED BY 'MySecurePass123';
-GRANT ALL PRIVILEGES ON uservault.* TO 'vaultuser'@'localhost';
-FLUSH PRIVILEGES;
+example
